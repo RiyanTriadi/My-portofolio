87966be7407de63086a3c78c12d100a7b70a2edb
RiyanTriadi <riyantriadii@com.com>
2025-12-19 23:24:53 +0700

    minor changes

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,14 +1,14 @@
-<div id="hero" class="hero min-h-screen" style="background-image: url(img/girlSmileLatern.jpg);">
+<div id="home" class="hero min-h-screen" style="background-image: url(img/girlSmileLatern.jpg);">
     <div class="hero-overlay"></div>
     <div class="hero-content text-neutral-content text-center">
         <div class="max-w-md">
-            <h2 class="text-3xl font-semibold">Hello there!</h2>
+            <h2 class="text-3xl font-semibold">Hello There!</h2>
             <h1 class="mb-5 text-6xl font-bold">I'M RIYAN</h1>
             <p class="mb-5">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                 quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
-            <button class="btn bg-olive-500 rounded-full border-none transition-all duration-300 transform hover:scale-110">See Projects</button>
+            <button class="btn bg-olive-500 rounded-full border-none transition-all duration-300 transform hover:bg-olive-600 hover:scale-110">See My Projects</button>
         </div>
     </div>
 </div>
