b87b689df5f63e9545aa923b71f887f883338e32
RiyanTriadi <riyantriadii@com.com>
2025-12-21 19:36:37 +0700

    adjust typography

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,14 +1,15 @@
-<div id="home" class="hero min-h-screen" style="background-image: url(img/girlSmileLatern.jpg);">
+<div id="home" class="hero min-h-screen w-screen" style="background-image: url(img/girlSmileLatern.jpg);">
     <div class="hero-overlay"></div>
     <div class="hero-content text-neutral-content text-center">
-        <div class="max-w-md">
+        <div class=" max-w-screen">
             <h2 class="text-3xl font-semibold">Hello There!</h2>
-            <h1 class="mb-5 text-6xl font-bold">I'M RIYAN</h1>
-            <p class="mb-5">
+            <h1 class="mb-5 text-5xl md:text-6xl font-bold">I'M RIYAN</h1>
+            <p class="mb-5 w-[85%] m-auto text-sm md:text-lg">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                 quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
-            <button class="btn bg-olive-500 rounded-full border-none transition-all duration-300 transform hover:bg-olive-600 hover:scale-110">See My Projects</button>
+            <button class="btn bg-olive-500 mx-2 rounded-full border-none transition-all duration-300 transform hover:bg-olive-600 hover:scale-110">View Projects</button>
+            <button class="btn btn-ghost mx-2 rounded-full border-olive-500 transition-all duration-300 transform hover:bg-transparent hover:shadow-none">Get In Touch</button>
         </div>
     </div>
 </div>
