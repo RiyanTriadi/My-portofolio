762ff754b64241f71f082a15d78f2d2868af0520
RiyanTriadi <riyantriadii@com.com>
2025-12-22 21:12:25 +0700

    add ubuntu font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     @vite('resources/css/app.css')
 </head>
-<body class="text-olive-100 bg-olive-700">
+
+<body class="text-olive-800 bg-olive-100">
     @include('components.navbar')
     <main>
         @yield('content')
     </main>
 </body>
+
 </html>
