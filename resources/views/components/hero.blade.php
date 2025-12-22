11805f98af43a2ea773ec3d2b089d5a2272b0d62
RiyanTriadi <riyantriadii@com.com>
2025-12-22 22:02:29 +0700

    adjust CTA font size in mobile

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,15 +1,13 @@
-<div id="home" class="hero min-h-screen w-screen" style="background-image: url(img/girlSmileLatern.jpg);">
-    <div class="hero-overlay"></div>
-    <div class="hero-content text-neutral-content text-center">
-        <div class=" max-w-screen">
-            <h2 class="text-3xl font-semibold">Hello There!</h2>
-            <h1 class="mb-5 text-5xl md:text-6xl font-bold">I'M RIYAN</h1>
-            <p class="mb-5 w-[85%] m-auto text-sm md:text-lg">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button class="btn bg-olive-500 mx-2 rounded-full border-none transition-all duration-300 transform hover:bg-olive-600 hover:scale-110">View Projects</button>
-            <button class="btn btn-ghost mx-2 rounded-full border-olive-500 transition-all duration-300 transform hover:bg-transparent hover:shadow-none">Get In Touch</button>
+<section id="home" class="min-h-screen w-screen relative text-center">
+    <div class=""></div>
+    <div class="max-w-300 w-[80%] lg:w-full h-screen m-auto flex flex-col items-center justify-center gap-5 ">
+        <h1 class="text-4xl md:text-6xl font-semibold">HELLO! I'M RIYAN TRIADI</h1>
+        <P class="text-sm md:text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, optio nihil ad, ipsam, perspiciatis
+            voluptatibus beatae cupiditate repudiandae quasi numquam dolorem est dolor cum. Architecto consequatur quis
+            assumenda tempore soluta?</P>
+        <div class="flex gap-3 text-sm md:text-md">
+            <a href="" class="py-2 px-3 md:py-3 md:px-4 rounded-full bg-olive-600 text-olive-100 transform duration-300 hover:bg-olive-700 hover:scale-110">View Works</a>
+            <a href="" class="py-2 px-3 md:py-3 md:px-4 rounded-full border text-olive-600 border-olive-600 transform duration-300 hover:bg-olive-600 hover:text-olive-100">Get in Touch</a>
         </div>
     </div>
-</div>
+</section>
